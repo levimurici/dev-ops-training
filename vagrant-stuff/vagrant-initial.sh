11562be8c98a992a219fd702dc9d4f1a2018e30a
Levi Murici <levi.murici@gmail.com>
2021-10-16 18:37:43 -0300

    Alterações no vagrant stuff

--- vagrant-stuff/vagrant-initial.sh
+++ vagrant-stuff/vagrant-initial.sh
@@ -1,41 +1,12 @@
-#!/bin/bash
 sudo apt-get update
-echo '--------------------------------'
-echo '--------------------------------'
-echo '--------------------------------'
-echo '--------------------------------'
-echo '--------------------------------'
 sudo apt-get install virtualbox
-echo '--------------------------------'
-echo '--------------------------------'
-echo '--------------------------------'
-echo '--------------------------------'
-echo '--------------------------------'
 sudo apt-get install virtualbox—ext–pack
-echo '--------------------------------'
-echo '--------------------------------'
-echo '--------------------------------'
-echo '--------------------------------'
-echo '--------------------------------'
 wget –q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- |
     sudo apt-key add -
-echo '--------------------------------'
-echo '--------------------------------'
-echo '--------------------------------'
-echo '--------------------------------'
-echo '--------------------------------'
 wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo
 apt-key add –
-echo '--------------------------------'
-echo '--------------------------------'
-echo '--------------------------------'
-echo '--------------------------------'
-echo '--------------------------------'
 sudo apt update
-echo '--------------------------------'
-echo '--------------------------------'
-echo '--------------------------------'
-echo '--------------------------------'
-echo '--------------------------------'
 sudo apt install -y vagrant
-sudo apt-get install -y virtualbox-guest-x11   
+sudo apt-get install -y virtualbox-guest-x11
+sudo apt-get install linux-headers-generic
+sudo apt-get install virtualbox-guest-additions-iso
