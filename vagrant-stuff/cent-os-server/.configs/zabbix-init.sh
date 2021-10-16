273034dfc3213a0768e88bb6868e57c8b02799fb
Levi Murici <levi.murici@gmail.com>
2021-10-16 15:35:58 -0300

    Alterações no vagrant stuff

--- vagrant-stuff/cent-os-server/.configs/zabbix-init.sh
+++
@@ -1,5 +0,0 @@
-sudo rpm -Uvh https://repo.zabbix.com/zabbix/5.4/rhel/7/x86_64/zabbix-release-5.4-1.el7.noarch.rpm
-sudo yum clean all
-sudo yum install -y zabbix-agent
-sudo systemctl restart zabbix-agent
-sudo systemctl enable zabbix-agent
