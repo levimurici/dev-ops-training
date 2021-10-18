9677f11d95d12057c7e55cc3d6974e303c0cf8f2
Levi Murici <levi.murici@gmail.com>
2021-10-18 09:23:59 -0300

    Alterações no vagrant stuff

--- vagrant-stuff/ubuntu-os-server/.configs/scripts/zabbix-script.sh
+++ vagrant-stuff/ubuntu-os-server/.configs/scripts/zabbix-script.sh
@@ -1,4 +1,4 @@
-sudo wget https://repo.zabbix.com/zabbix/4.4/ubuntu/pool/main/z/zabbix-release/zabbixrelease_4.4-1+bionic_all.deb
-sudo dpkg -i zabbix-release_4.4-1+bionic_all.deb
+sudo wget wget https://repo.zabbix.com/zabbix/5.4/ubuntu/pool/main/z/zabbix-release/zabbix-release_5.4-1+ubuntu18.04_all.deb
+sudo dpkg -i zabbix-release_5.4-1+ubuntu18.04_all.deb
 sudo apt update
-sudo apt -y install zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbixagent
+sudo apt -y install zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-sql-scripts zabbix-agent
